f45d021c77cf1d3796669c7d9fb97a71521d32bf
Ajaybihari759 <115182437+Ajaybihari759@users.noreply.github.com>
2022-10-18 21:26:43 +0530

    Print_Natural_Number
    Enter the number
    10
    1  2  3  4  5  6  7  8  9  10

---
+++ NaturalNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NaturalNo {
+
+    public static void main(String [] args){
+    Scanner ob=new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int a=ob.nextInt();
+
+            for(int i=1; i<=a; i++){
+            System.out.print(i + "  ");
+                    
+        }
+    }
+}

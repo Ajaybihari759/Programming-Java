4a4b97b50f381ce9980430395d691cdadbf35fcc
Ajaybihari759 <115182437+Ajaybihari759@users.noreply.github.com>
2022-10-18 23:11:48 +0530

    Check Factorial
    Enter your Fact no:
    5
    120

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+             System.out.println("Enter your Fact no:");
+                 int a,b=1;
+                    a = ob.nextInt();
+                        for (int i=1; i<=a; i++){
+                           b=b*i;
+                             }
+                                System.out.println(b);
+                               }
+                          }

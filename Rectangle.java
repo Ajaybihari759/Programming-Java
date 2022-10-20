b346da0a21bdc87b9da8dd8dcfd21cc42de2fde7
Ajaybihari759 <115182437+Ajaybihari759@users.noreply.github.com>
2022-10-20 21:20:02 +0530

    Calculate of Area Rectangle
    ajay@ajay-HP-Laptop-15-di0xxx:~/Desktop$ javac Rectangle.java
    ajay@ajay-HP-Laptop-15-di0xxx:~/Desktop$ java Rectangle
    Enter value for side: 
    5
    6
    Area of Rectangle: 30

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+           // Calculate of Area Rectangle
+  import java.util.Scanner;
+   class Rectangle{
+    public static void main(String[]args){
+     System.out.println("Enter value for side: ");
+      int i,b,area;  // l for Lenth, b for Birth
+      Scanner ob = new Scanner(System.in);
+     i=ob.nextInt();
+    b=ob.nextInt();
+   area = i*b;
+  System.out.println("Area of Rectangle: "+ area);     
+     }
+      }

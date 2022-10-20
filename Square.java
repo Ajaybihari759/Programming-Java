a45883b81d437255ba45a3a694e3e2ea55dc629c
Ajaybihari759 <115182437+Ajaybihari759@users.noreply.github.com>
2022-10-20 22:02:32 +0530

    Area of Square program
    Enter the side value:
    5
    Area of square: 25

---
+++ Square.java
@@ -0,0 +1,12 @@
+            // Area of Square program
+ import java.util.Scanner;
+   class Square{
+     public static void main(String[]arg){
+       int s,area;   // S for Square
+         System.out.println("Enter the side value:");
+         Scanner ob = new Scanner(System.in);
+       s = ob.nextInt();
+     area = s*s;   // Formula of Area Square :- area=square*square
+   System.out.println("Area of square: "+area);  
+ }
+   }
